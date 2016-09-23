c81f1e4a7d5b2ad7a85b8cf2acd1ce53fcbf2486
eriche2016 <1154646635@qq.com>
2016-09-23 17:02:17 +0800

    Update Attention_Weights_Criterion.lua

--- misc_saver2_reg_atten_ws/Attention_Weights_Criterion.lua
+++ misc_saver2_reg_atten_ws/Attention_Weights_Criterion.lua
@@ -6,8 +6,10 @@
 
 local Attention_Weights_Criterion, parent = torch.class('nn.Attention_Weights_Criterion', 'nn.Criterion')
 
-function  Attention_Weights_Criterion:__init() 
-    parent.__init(self)     
+function  Attention_Weights_Criterion:__init(eps) 
+    parent.__init(self)
+    -- it is very import to avoid dividing zero problem 
+    self.eps = eps or 1e-5 -- by default self.alpha is set to be 0.01 
 end 
 
 
@@ -15,19 +17,26 @@ end
 -- target is nil currently and no use 
 function  Attention_Weights_Criterion:updateOutput(input, target) 
     assert(input:dim() == 3)
+
     local bz, L, top_attrs = unpack(input:size():totable()) 
-    local sum_power_along_L = torch.pow(torch.sum(input, input:dim()-1), 2) -- bz x 1 x top_attrs 
-    local sum_sqrt_along_top = torch.pow(torch.sum(sum_power_along_L, input:dim()), 0.5) -- bz x 1 x 1  
-    -- squeeze method will remove all singleton dimenstion 
-    sum_sqrt_along_top = sum_sqrt_along_top:squeeze() -- bz  
 
-    local sqrt_sum_top = torch.sum(torch.pow(input, 0.5), input:dim()) 
-    local power_sum_along_L = torch.sum(torch.pow(sqrt_sum_top, 2), input:dim() -1) -- bz x 1 x 1 
-    -- squeeze singleton dimention  
-    power_sum_along_L = power_sum_along_L:squeeze() -- bz  
+    -- fist term 
+    -- sum along L 
+    local sum_along_L = torch.sum(input, input:dim()-1) -- bz x 1 x top_attrs 
+    local pow_sum_L = sum_along_L:pow(2) 
+    local sum_top_pow_sum_L = torch.sum(pow_sum_L, input:dim()) 
+    local sqrt_sum_top_pow_sum_L = sum_top_pow_sum_L:sqrt() -- bz x 1 x 1 
 
-    -- self.output is a value 
-    self.output = torch.sum(sum_sqrt_along_top + power_sum_along_L) 
+    --second term 
+    local sqrt_input = torch.sqrt(input + self.eps) 
+
+    local sum_along_top = torch.sum(sqrt_input, input:dim())
+    local pow_sum_along_top = sum_along_top:pow(2) 
+    local sum_L_pow_sum_top = torch.sum(pow_sum_along_top, input:dim()-1) -- bz x 1 x 1  
+    
+    -- final results 
+    self.output = torch.sum(sqrt_sum_top_pow_sum_L+sum_L_pow_sum_top) 
+
     return self.output 
 end 
 
@@ -38,18 +47,33 @@ function Attention_Weights_Criterion:updateGradInput(input, target)
     -- difficulities: how to compute the gradients w.r.t input 
     self.gradInput:resizeAs(input) -- bz x L x top_attrs
 
-    -- for statements may be slow but works
     for b = 1, self.gradInput:size(1) do  -- iterate over batch data of size bz 
+        local sum_L = torch.sum(input[b], input[b]:dim()-1) 
+        local pow_sum_L = sum_L:pow(2) 
+        local sum_top_pow_sum_L =  torch.sum(pow_sum_L, pow_sum_L:dim())
+       
+        local multiplier11 = sum_top_pow_sum_L:pow(-0.5) -- 1 x 1  
+        local multiplier12 = torch.sum(input[b], input[b]:dim()-1) -- 1 x top_attrs 
+       
+        local sqrt_input = torch.sqrt(input[b] + self.eps) 
+        local multiplier21 = torch.sum(sqrt_input, sqrt_input:dim()) -- L x 1  
+        local multiplier22 = sqrt_input:cinv()  -- L * top_attrs  -- inplace 
+
+        local first_term = torch.cmul(multiplier11:expandAs(input[b]), multiplier12:expandAs(input[b]))
+        -- error code, remains here to avoid my self that the following doesnot work
+        -- local second_term = torch.cmul(multiplier21:expandAs(input[b]):cmul(multiplier22)
+        local second_term = torch.cmul(multiplier21:expandAs(input[b]), multiplier22)
+       
+        --[[
         local first_term = torch.pow(torch.sum(torch.pow(torch.sum(input[b], 1), 2), 2), -0.5) * torch.sum(input[b], 1)
         first_term = first_term:expandAs(input[b])
         local second_term = torch.cmul(torch.sum(torch.pow(input[b], 0.5), 2):expandAs(input[b]), torch.pow(input[b], -0.5))
-
-        self.gradInput[b] = first_term:expandAs(input[b]) + second_term
+        --]]
+        self.gradInput[b]:copy(first_term):add(second_term)
     end
     
     -- not worked!!!, may check it later
     --[[
-    --
     -- 1st step:
     local first_term_1 = torch.pow(torch.sum(torch.pow(torch.sum(input, input:dim()-1), 2), input:dim()), -0.5)-- bz x 1 x 1  
     -- 2nd step: 
